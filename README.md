6d14794543a9bee13291004aa2dc71462232b538
Vadim Komarov <vadim.y.komarov@gmail.com>
2021-12-11 11:18:22 +0500

    Initial

---
+++ README.md
@@ -0,0 +1,3 @@
+# aoc2021
+
+[Advent of code](https://adventofcode.com) 2021 in Racket.
---
+++ info.rkt
@@ -0,0 +1,8 @@
+#lang info
+(define collection "aoc2021")
+(define deps '("base" "threading"))
+(define build-deps '("rackunit-lib"))
+(define pkg-desc "Advent of code 2021")
+(define version "0.1")
+(define pkg-authors '(vko))
+(define license '(MIT))
